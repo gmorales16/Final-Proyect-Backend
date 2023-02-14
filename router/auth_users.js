f8d4a12fddc1fe76479a98475ff3755477781e44
Guillermo Morales <guillermo.gazille@gmail.com>
2023-02-14 00:17:36 -0300

    first commit

---
+++ router/auth_users.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+let books = require("./booksdb.js");
+const app = express();
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
+
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
